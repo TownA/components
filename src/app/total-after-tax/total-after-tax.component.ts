bd22c5591ad5ee1cb6a65a49d1ff086da9eae2ad
Austin Brown <austinnbrown95@gmail.com>
2023-03-16 15:55:15 -0500

    Fixed (click) handler

--- src/app/total-after-tax/total-after-tax.component.ts
+++ src/app/total-after-tax/total-after-tax.component.ts
@@ -5,17 +5,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './total-after-tax.component.html',
   styleUrls: ['./total-after-tax.component.css']
 })
+
+
+
 export class TotalAfterTaxComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
-
+    
   }
 
   comments = "We want to hear from you!";
 
   commentsSubmitted = () => {
-    this.comments = "Thank you for submitting your comments!"
+    this.comments = "Thank you for submitting your comments!";
   }
 }
